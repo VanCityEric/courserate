e5fb113c47ae776e2596e2ab3983ee27c26dce89
Eric Coscolluela <65707641+VanCityEric@users.noreply.github.com>
2022-02-27 00:46:47 -0800

    fix database issues

--- client/src/components/NewPostForm.js
+++ client/src/components/NewPostForm.js
@@ -115,11 +115,11 @@ const NewPostForm = ({
         body: JSON.stringify(body)
       });
 
-      const responsetwo = await fetch("/api/averagesinsert", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(body)
-      });
+      // const responsetwo = await fetch("/api/averagesinsert", {
+      //   method: "POST",
+      //   headers: { "Content-Type": "application/json" },
+      //   body: JSON.stringify(body)
+      // });
 
       window.location = "/";
     } catch (err) {
