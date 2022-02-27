603a2e706fa4571e0102fb145f8a07e8deb0f2b2
Eric Coscolluela <65707641+VanCityEric@users.noreply.github.com>
2022-02-26 18:29:14 -0800

    fixed database issues

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ const pool = new Pool({
 });
 
 
-
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}))
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
