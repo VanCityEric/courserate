ce7f2298977eeeb22fccea813d173aca6945f161
Eric Coscolluela <65707641+VanCityEric@users.noreply.github.com>
2022-02-26 22:16:28 -0800

    fixed database issues

--- index.js
+++ index.js
@@ -89,9 +89,9 @@ app.post("/api/insert", (req, res) => {
   try {
     console.log(req.body);
     const { courseName } = req.body;
-    const { numbery } = req.body;
+    const { courseNumber } = req.body;
     pool.query(
-      `INSERT INTO todo (description, number) VALUES ('${courseName}', 233)`,
+      `INSERT INTO todo (description, number) VALUES ('${courseName}', ${courseNumber})`,
     );
   } catch (err) {
     console.error(err.message);
